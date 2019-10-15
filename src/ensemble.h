96450e985029b0f44aab04b9691ce83345fca287
Neal <735151009@qq.com>
2019-10-15 10:36:51 +0800

    delete integral & append kinetic and potential

--- src/ensemble.h
+++ src/ensemble.h
@@ -24,25 +24,30 @@ public:
         return ensemble[index];
     }
 
-    // calculate the total acceleration from other particles
+    // calculate the total acceleration from other particles (ensemble version)
     void interact(Particle& particle) {
         particle.a_x = 0;
         particle.a_y = 0;
         particle.a_z = 0;
         double distance_value = 0;
         for (auto particle_ptr = ensemble.begin(); particle_ptr != ensemble.end(); ++particle_ptr) {
+            // exclude the current particle
             if (&(*particle_ptr) != &particle) {
                 distance_value = sqrt(
                     pow((particle_ptr->pos_x - particle.pos_x), 2) + 
                     pow((particle_ptr->pos_y - particle.pos_y), 2) +
                     pow((particle_ptr->pos_z - particle.pos_z), 2));
 
-                particle.a_x += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - 
+                // particle.a_x += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - 
+                //     6 * pow(particle.sigma, 6) / pow(distance_value, 7)) * (particle_ptr->pos_x - particle.pos_x) / particle.mass;
+                // particle.a_y += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - 
+                //     6 * pow(particle.sigma, 6) / pow(distance_value, 7)) * (particle_ptr->pos_y - particle.pos_y) / particle.mass;
+                // particle.a_z += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - 
+                //     6 * pow(particle.sigma, 6) / pow(distance_value, 7)) * (particle_ptr->pos_z - particle.pos_z) / particle.mass;
+
+                // another version of F(r) function to see if the speed is faster than before
+                particle.a_x += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - ~
                     6 * pow(particle.sigma, 6) / pow(distance_value, 7)) * (particle_ptr->pos_x - particle.pos_x) / particle.mass;
-                particle.a_y += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - 
-                    6 * pow(particle.sigma, 6) / pow(distance_value, 7)) * (particle_ptr->pos_y - particle.pos_y) / particle.mass;
-                particle.a_z += -4 * particle.epsilon * (12 * pow(particle.sigma, 12) / pow(distance_value, 13) - 
-                    6 * pow(particle.sigma, 6) / pow(distance_value, 7)) * (particle_ptr->pos_z - particle.pos_z) / particle.mass;
             }
         }
     }
--- src/particle.h
+++ src/particle.h
@@ -2,10 +2,8 @@
 #define PARTICLE_H
 
 #include <fstream>
-#include <vector>
 #include <cmath>
 #include <stdexcept>
-#include "integral.h"
 
 using namespace std;
 
@@ -31,25 +29,31 @@ public:
     Particle(const Particle& other): v_x(other.v_x), v_y(other.v_y), v_z(other.v_z), pos_x(other.pos_x), pos_y(other.pos_y), pos_z(other.pos_z),
         mass(other.mass), epsilon(other.epsilon), sigma(other.sigma), time_interval(other.time_interval) { }
 
-    // check if two particles are the same
-    // bool operator==(const Particle &other) {
-    //     return pos_x == other.pos_x && pos_y == other.pos_y && pos_z == other.pos_z &&
-    //             v_x == other.v_x && v_y == other.v_y && v_z == other.v_z &&
-    //             a_x == other.a_x && a_y == other.a_y && a_z == other.a_z;
-    // }
-
-    // calculate acceleration of the particle from interaction
-    void interact(const Particle& other) {
-        double distance_value = sqrt(
+    // calculate the currenct distance between two particle
+    void calculate_distance_value(const Particle& other) {
+        distance_value = sqrt(
             pow((pos_x - other.pos_x), 2) + 
             pow((pos_y - other.pos_y), 2) +
             pow((pos_z - other.pos_z), 2));
-        a_x = -4 * epsilon * (12 * pow(sigma, 12) / pow(distance_value, 13) - 
-            6 * pow(sigma, 6) / pow(distance_value, 7)) * (other.pos_x - pos_x) / mass;
-        a_y = -4 * epsilon * (12 * pow(sigma, 12) / pow(distance_value, 13) - 
-            6 * pow(sigma, 6) / pow(distance_value, 7)) * (other.pos_y - pos_y) / mass;
-        a_z = -4 * epsilon * (12 * pow(sigma, 12) / pow(distance_value, 13) - 
-            6 * pow(sigma, 6) / pow(distance_value, 7)) * (other.pos_z - pos_z) / mass;
+    }
+
+    // calculate acceleration of the particle from interaction (particle version)
+    void interact(const Particle& other) {
+        calculate_distance_value(other);
+        // a_x = -4 * epsilon * (12 * pow(sigma, 12) / pow(distance_value, 13) - 
+        //     6 * pow(sigma, 6) / pow(distance_value, 7)) * (other.pos_x - pos_x) / mass;
+        // a_y = -4 * epsilon * (12 * pow(sigma, 12) / pow(distance_value, 13) - 
+        //     6 * pow(sigma, 6) / pow(distance_value, 7)) * (other.pos_y - pos_y) / mass;
+        // a_z = -4 * epsilon * (12 * pow(sigma, 12) / pow(distance_value, 13) - 
+        //     6 * pow(sigma, 6) / pow(distance_value, 7)) * (other.pos_z - pos_z) / mass;
+
+        // another version of F(r) function to see if the speed is faster than before
+        a_x = -4 * epsilon * (12 * pow(sigma / distance_value, 12) / distance_value - 
+            6 * pow(sigma / distance_value, 6) / distance_value) * (other.pos_x - pos_x) / mass;
+        a_y = -4 * epsilon * (12 * pow(sigma / distance_value, 12) / distance_value - 
+            6 * pow(sigma / distance_value, 6) / distance_value) * (other.pos_y - pos_y) / mass;
+        a_z = -4 * epsilon * (12 * pow(sigma / distance_value, 12) / distance_value - 
+            6 * pow(sigma / distance_value, 6) / distance_value) * (other.pos_z - pos_z) / mass;
     }
 
     // output to file
@@ -75,23 +79,40 @@ public:
         v_z += a_z * time_interval;
     }
 
+    // calculate the current kinetic energy of the particle
+    double kinetic() const {
+        return 0.5 * mass * sqrt( pow(v_x, 2) + pow(v_y, 2) + pow(v_z, 2) );
+    }
+
+    // calculate potential between two particles (particle version)
+    // note: must call "interact" before calculate the potential
+    double potential(const Particle& other) {
+        return 4 * epsilon * ( pow(sigma / distance_value, 12) - pow(sigma / distance_value, 6) );
+    }
+
+    // calculate total energy (particle version)
+    double total_energy(const Particle& other) {
+        return kinetic() + potential(other);
+    }
+
+
 protected:
+    // position
+    double pos_x;
+    double pos_y;
+    double pos_z;
 
     // velocity
     double v_x;
     double v_y;
     double v_z;
 
-    // position
-    double pos_x;
-    double pos_y;
-    double pos_z;
-
-
     // acceleration
     double a_x;
     double a_y;
     double a_z;
+
+    double distance_value;
 
     double mass;
 
@@ -102,78 +123,5 @@ protected:
     double time_interval;
 
 };
-
-
-
-// class Particle_Energy_Corrected: public Particle {
-// public:
-
-//     // calculate potential between two particles
-//     double potential(const double &_pos_x, const double &_pos_y, const double &_pos_z, 
-//         const double &_other_pos_x, const double &_other_pos_y, const double &_other_pos_z) {
-
-//         double distance_value = sqrt(
-//             pow((_pos_x - _other_pos_x), 2) + 
-//             pow((_pos_y - _other_pos_y), 2) +
-//             pow((_pos_z - _other_pos_z), 2));
-        
-//         if (distance_value <= sigma) {
-//             // calculate the intergration in (r, sigma)
-//             return integral(epsilon, sigma, distance_value, sigma);
-//         } else {
-//             return -1 * integral(epsilon, sigma, sigma, distance_value);
-//         }
-//     }
-
-//     // calculate kinetic energy of the particle
-//     double kinetic(const double &_v_x, const double &_v_y, const double &_v_z) {
-//         return 0.5 * mass * sqrt( pow(_v_x, 2) + pow(_v_y, 2) + pow(_v_z, 2) );
-//     }
-
-//     // calculate the velocity according to conservation of energy 
-//     void interact(const Particle_Energy_Corrected& other) {
-//         double velocity = sqrt(
-//             (kinetic(former_v_x, former_v_y, former_v_z) + 
-//             potential(former_pos_x, former_pos_y, former_pos_z, other.former_pos_x, other.former_pos_y, other.former_pos_z) -
-//             potential(pos_x, pos_y, pos_z, other.pos_x, other.pos_y, other.pos_z) ) * 2 / mass
-//         );
-
-//         double velocity_to_be_corrected = sqrt(pow(v_x, 2) + pow(v_y, 2) + pow(v_z, 2));
-//         v_x = velocity * v_x / velocity_to_be_corrected;
-//         v_y = velocity * v_y / velocity_to_be_corrected;
-//         v_z = velocity * v_z / velocity_to_be_corrected;
-//     }
-
-//     // calculate movement in given time granularity
-//     void movement() {
-//         // record former position and velocity
-//         former_pos_x = pos_x;
-//         former_pos_y = pos_y;
-//         former_pos_z = pos_z;
-
-//         former_v_x = v_x;
-//         former_v_y = v_y;
-//         former_v_z = v_z;
-
-//         // uniformly accelerated motion
-//         pos_x += v_x * time_interval + 0.5 * a_x * pow(time_interval, 2);
-//         pos_y += v_y * time_interval + 0.5 * a_y * pow(time_interval, 2);
-//         pos_z += v_z * time_interval + 0.5 * a_z * pow(time_interval, 2);
-//         v_x += a_x * time_interval;
-//         v_y += a_y * time_interval;
-//         v_z += a_z * time_interval;
-//     }
-
-// private:
-//     // former position
-//     double former_pos_x;
-//     double former_pos_y;
-//     double former_pos_z;
-
-//     //former velocity
-//     double former_v_x;
-//     double former_v_y;
-//     double former_v_z;
-// };
 
 #endif
